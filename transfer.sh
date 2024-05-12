ee464c2b0d88ce75084729d4d70de7a5f329a9f0
jiaqi <lywhlao@163.com>
2024-05-12 09:53:32 +0800

    feat:transfer.sh

---
+++ transfer.sh
@@ -0,0 +1,22 @@
+# 定义源文件夹和目标IP
+source_folder="/home/pi"
+destination_ip="100.100.133.23"
+
+# 查找以.jpe结尾的文件
+files=$(find "$source_folder" -type f -name "*.jpg")
+
+# 循环处理每个找到的文件
+for file in $files; do
+    # 通过scp传输文件到目标机器
+    scp "$file" root@$destination_ip:/usr/share/nginx/html
+
+    # 检查传输是否成功
+    if [ $? -eq 0 ]; then
+        echo "文件 $file 传输成功."
+        # 传输成功后，删除文件
+        rm "$file"
+        echo "文件 $file 删除成功."
+    else
+        echo "文件 $file 传输失败."
+    fi
+done
